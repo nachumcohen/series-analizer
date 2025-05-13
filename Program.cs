53c0bc42809f29448e40dbd8ef56eac041fe67ef
nachum <nahum0583@gmail.com>
2025-05-13 16:33:42 +0300

    finish funk

--- Program.cs
+++ Program.cs
@@ -32,108 +32,7 @@ namespace menu_numbers
         //    return ifSize;
         //}
 
-        //chack if number and converter if string in number
-        static int ifNum(string ifnum)
-        {
-            if (int.TryParse(ifnum, out _)){
-                return converstrNum(ifnum);
-            }
-            return 0;
-        }
-
-        //Coverts string to number
-        static int converstrNum(string num)
-        {
-            int x = int.Parse(num);
-            return x;
-            
-        }
-
-        //Checks it the number is positive
-        static bool ifPositive(int num)
-        {
-            if (num <= 0)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        //retures all list is positive
-        static bool ifListNuPos(string[] arge)
-        {   
-            bool ifAllNum = true;
-            foreach (string str in arge)
-            {
-                if (!ifPositive(ifNum(str)))
-                {
-                    ifAllNum = false;
-                }
-            
-            }
-            return ifAllNum;
-        }
-
-        //Creates a list at least 3 numbers until the user stoped.
-        static List<int> threeNum(List<int> listInt)
-        {
-            int min = 0;
-            string stop = "";
-
-            while (min < 3 || stop != "stop")
-            {
-                Console.WriteLine("pleas enter at least 3 number");
-                string input = Console.ReadLine();
-                if (ifPositive(ifNum(input)))
-                {
-                    listInt.Add(converstrNum(input));
-                    min++;
-                }
-
-                if (!ifPositive(ifNum(input)))
-                {
-                    Console.WriteLine("pleas enter only number and positive");
-                }
-
-                if (min >= 3)
-                {
-                    Console.WriteLine("if you want to stop write 'stop' ");
-                    stop = Console.ReadLine();
-                }
-
-                
-
-            }
-            return listInt;
-
-        }
-
-
-        /*
-         Returns a finish list of numbers
-         */
-        static List<int> converAegsListInt(string[] arge)
-        {
-            List<int> listInt = new List<int>();
-            if (arge.Length < 3)
-            {
-                listInt = threeNum(listInt);
-            }
-            else if (!ifListNuPos(arge))
-            {
-                listInt = threeNum(listInt);
-            }
-            else
-            {
-                foreach (string str in arge)
-                {
-                    listInt.Add(converstrNum(str));
-                }
-            }
-            return listInt;
-        }
-
-        static void menu()
+        static void Menu()
         {
             Console.WriteLine(@"
             Choose one of the option
@@ -150,7 +49,7 @@ namespace menu_numbers
         }
 
         //makes sure in choos
-        static bool ifChoosInMenu(string choos)
+        static bool IfChoosInMenu(string choos)
         {   
             bool choosTrue = false;
             switch(choos)
@@ -189,56 +88,53 @@ namespace menu_numbers
             return choosTrue;
         }
 
-
-
-
-        static void menuFunc(string choos , List<int> listi)
+        static void MenuFunc(string choos , List<int> listi)
         {
            
             switch (choos)
             {
                 case "1":
-                    changeListInt(listi);
+                    ChangeListInt(listi);
                     break;
                 case "2":
-                    showList(listi);
+                    ShowList(listi);
                     break;
                 case "3":
-                    //display reverse();
+                    ShowReverseList(listi);
                     break;
                 case "4":
-                    //display sort();
+                    SortList(listi);
                     break;
                 case "5":
-                    Console.WriteLine(maxList(listi));
+                    Console.WriteLine(MaxList(listi));
                     break;
                 case "6":
-                    Console.WriteLine(valuyeMinList(listi));
+                    Console.WriteLine(ValuyeMinList(listi));
                     break;
                 case "7":
-                    Console.WriteLine(avargeOfNumList(listi));
+                    Console.WriteLine(AvargeOfNumList(listi));
                     break;
                 case "8":
-                    Console.WriteLine(numElements(listi));
+                    Console.WriteLine(NumElements(listi));
                     break;
                 case "9":
-                    Console.WriteLine(sumList(listi));
+                    Console.WriteLine(SumList(listi));
                     break;
             }
             
         }
 
         //change list of int
-        static void changeListInt(List<int> listi)
+        static void ChangeListInt(List<int> listi)
         {
             
             listi.Clear();
-            threeNum(listi);
+            ThreeNum(listi);
             
         }
 
         //show all values
-        static void showList(List<int> show)
+        static void ShowList(List<int> show)
         {
             foreach (int i in show)
             {
@@ -246,19 +142,54 @@ namespace menu_numbers
             }
         }
 
-        //static void showReverseList(List<int> reverse)
-        //{
+        //printer a revers list
+        static void ShowReverseList(List<int> reverse)
+        {
+            int x = reverse.Count;
+            for (int i = 0; i < reverse.Count; i++)
+            {
+                x--;
+                Console.Write(reverse[x]);
+            }
+        }
 
-        //}
+        //sorted lists
+        static void SortList(List<int> sortlist)
+        {
+            List<int> list = new List<int>(sortlist);
+            for (int i = 0; i < sortlist.Count; i++)
+            {  
+                bool ifnotsort = true;
+                for (int j = 0; j < list.Count - 1; j++)
+                {
+                    int value = list[j];
+                    int value2 = list[j + 1];
+                    if (value > value2)
+                    {
+                        list[j] = value2;
+                        list[j + 1] = value;
+                        ifnotsort = false;
+                    }
+                }
+                if (ifnotsort)
+                {
+                    break;
+                }
+            }
+            foreach(int i in list)
+            {
+                Console.Write(i);
+            }
+            
+            
+            
+            
 
-        //static List<int> sortList(List<int> sortlist)
-        //{
-           
-        //}
+        }
 
 
         //print num max from list
-        static int maxList(List<int> maxlist)
+        static int MaxList(List<int> maxlist)
         {
             int max = 0;
             foreach (int i in maxlist)
@@ -272,7 +203,7 @@ namespace menu_numbers
         }
 
         //print num min from list
-        static int valuyeMinList(List<int> minlist)
+        static int ValuyeMinList(List<int> minlist)
         {
             int min = minlist[0];
             foreach (int i in minlist)
@@ -285,17 +216,17 @@ namespace menu_numbers
             return min;
         }
 
-        static int avargeOfNumList(List<int> avelist)
+        static int AvargeOfNumList(List<int> avelist)
         {
-            return sumList(avelist) / numElements(avelist);
+            return SumList(avelist) / NumElements(avelist);
         }
 
-        static int numElements(List<int> numelem)
+        static int NumElements(List<int> numelem)
         {
             return numelem.Count;
         }
 
-        static int sumList(List<int> sumlist)
+        static int SumList(List<int> sumlist)
         {
             int sum = 0;
             foreach(int i in sumlist)
@@ -306,36 +237,129 @@ namespace menu_numbers
         }
 
 
-        
-        static void playMenu(List<int> listi)
+
+        //chack if number and converter if string in number
+        static int IfNum(string ifnum)
+        {
+            if (int.TryParse(ifnum, out int x))
+            {
+                return x;
+            }
+            return 0;
+        }
+
+        //Coverts string to number
+        //static int converstrNum(string num)
+        //{
+        //    int x = int.Parse(num);
+        //    return x;
+
+        //}
+
+        //Checks it the number is positive
+        static bool IfPositive(int num)
+        {
+            if (num <= 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //retures all list is positive
+        static bool IfListNuPos(string[] arge)
+        {
+            bool ifAllNum = true;
+            foreach (string str in arge)
+            {
+                if (!IfPositive(IfNum(str)))
+                {
+                    ifAllNum = false;
+                }
+
+            }
+            return ifAllNum;
+        }
+
+
+        //Creates a list at least 3 numbers until the user stoped.
+        static List<int> ThreeNum(List<int> listInt)
+        {
+            int min = 0;
+            string stop = "";
+
+            while (min < 3 || stop != "stop")
+            {
+                Console.WriteLine("pleas enter at least 3 number");
+                string input = Console.ReadLine();
+                if (IfPositive(IfNum(input)))
+                {
+                    listInt.Add(IfNum(input));
+                    min++;
+                }
+
+                if (!IfPositive(IfNum(input)))
+                {
+                    Console.WriteLine("pleas enter only number and positive");
+                }
+
+                if (min >= 3)
+                {
+                    Console.WriteLine("if you want to stop write 'stop' ");
+                    stop = Console.ReadLine();
+                }
+
+            }
+            return listInt;
+
+        }
+
+        /*
+         Returns a finish list of numbers
+         */
+        static List<int> ConverAegsListInt(string[] arge)
+        {
+            List<int> listInt = new List<int>();
+            if (arge.Length < 3)
+            {
+                listInt = ThreeNum(listInt);
+            }
+            else if (!IfListNuPos(arge))
+            {
+                listInt = ThreeNum(listInt);
+            }
+            else
+            {
+                foreach (string str in arge)
+                {
+                    listInt.Add(IfNum(str));
+                }
+            }
+            return listInt;
+        }
+
+
+        static void PlayMenu(List<int> listi)
         {
             string choos = "";
             while (choos != "10")
             {
-                menu();
+                Menu();
                 choos = Console.ReadLine();
-                if (!ifChoosInMenu(choos))
+                if (!IfChoosInMenu(choos))
                 {
                     Console.WriteLine("plese choos only 1-10");
                     continue;
                 }
-                menuFunc(choos , listi);
-
-
-
+                MenuFunc(choos , listi);
             }
         }
-
-
-
 
            
         static void Main(string[] args)
         {
-
-           playMenu(converAegsListInt(args));
+           PlayMenu(ConverAegsListInt(args));
            
-            
         }
     }
 }
